4ae32a58892006fcbf36f838b72815f846e98ee3
Michael Kechinov <sam@mkechinov.ru>
2013-07-20 11:09:07 +0400

    1.9.3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Shop2::Application.configure do
   #
   #Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
-  
+
   # Enable threaded mode
   # config.threadsafe!
 
